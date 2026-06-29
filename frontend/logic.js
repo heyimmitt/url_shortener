55c61ede6fcb488ede25fe222ddf87830a94b4be
anoushka <anoushka.basak03@gmail.com>
2026-06-30 00:47:22 +0530

    added requirements.txt

--- frontend/logic.js
+++ frontend/logic.js
@@ -6,8 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	form.addEventListener('submit', async (e) => {
 		e.preventDefault();
-		// console.log('Form submitted, default prevented');
-
+		
 		const url = longUrl.value.trim();
 		if (!url) {
 			output.innerHTML = 'Please enter a URL.';
